c2d2b11133eb45ed8e9679feb2f55c3a79cae31c
Krish Patel <krish.s.patel@icloud.com>
2024-07-09 19:57:45 -0500

    Added movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useEffect } from "react";
 
+import MovieCard from './MovieCard'
+
 import './App.css'
 import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com?apikey=69b70cc1'
+
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 const App = () => {
 
@@ -17,6 +27,7 @@ const App = () => {
     useEffect(() => {
         searchMovies('Spiderman')
     }, []);
+
     return (
         <div className="app">
             <h1>MovieLand</h1>
@@ -30,7 +41,12 @@ const App = () => {
                 <img
                     src={SearchIcon}
                     alt="search"
+                    onClick={() => { }}
                 />
+            </div>
+
+            <div className="container">
+                <MovieCard movie1={movie1} />
             </div>
         </div>
     );
