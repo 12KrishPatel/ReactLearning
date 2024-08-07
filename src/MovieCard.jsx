8d07b19cd3c6b8a550c3170fd6e584d75f896996
Krish Patel <krish.s.patel@icloud.com>
2024-08-07 16:24:40 -0500

    added rank with hovering movie card

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -10,8 +10,12 @@ const MovieCard = ({ movie }) => {
                 <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.l} />
             </div>
             <div>
-                <span>{movie.q}</span>
+                <span>{movie.qid}</span>
                 <h3>{movie.l}</h3>
+                <div className="hover-text">
+                    <span className="hover-content">Rank: {movie.rank}</span>
+                    <span className="hover-text">Hover to see my rank</span>
+                </div>
             </div>
         </div>
     )
