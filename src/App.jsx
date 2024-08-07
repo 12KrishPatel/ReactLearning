b3fa65dd5b8903435a1626f79de4d555ca5d73fc
Krish Patel <krish.s.patel@icloud.com>
2024-08-07 15:24:10 -0500

    first attempt at fullly adding IMDB api

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,42 @@ import './App.css'
 import SearchIcon from './search.svg'
 const imdbID = 'tt0145487'
 
-const API_URL = 'http://www.omdbapi.com?apikey=69b70cc1'
-const RATING_URL = `https://movies-ratings2.p.rapidapi.com/ratings?id=${imdbID}]`
+//const API_URL = 'http://www.omdbapi.com?apikey=69b70cc1'
+//const RATING_URL = `https://movies-ratings2.p.rapidapi.com/ratings?id=${imdbID}]`
+const IMDB_URL = `https://imdb-movies-web-series-etc-search.p.rapidapi.com/${searchTerm}+.json`
+const RAPIDAPI_HOST = 'imdb-movies-web-series-etc-search.p.rapidapi.com';
+const RAPIDAPI_KEY = '3fc9f8e71fmsh32bef10f957d078p1809d4jsn79aa39f6d468';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
+    // const searchMovies = async (title) => {
+    //     const response = await fetch(`${API_URL}&s=${title}`);
+    //     const data = await response.json();
+    //     setMovies(data.Search);
+    // }
+
+    // const [ratings, setRatings] = useState([]);
+
+    // const searchRatings = async (ratings) => {
+    //     const response = await fetch(`${RATING_URL}&s=${ratings}`);
+    //     const data = await response.json();
+    //     setRatings(data.Search);
+    // }
+
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
+        const response = await fetch(`${IMDB_URL}?s=${title}`, {
+            method: 'GET',
+            headers: {
+                'x-rapidapi_host': RAPIDAPI_HOST,
+                'x-rapidapi_key': RAPIDAPI_KEY
+            }
+        });
         const data = await response.json();
         setMovies(data.Search);
     }
 
-    const [ratings, setRatings] = useState([]);
-
-    const searchRatings = async (ratings) => {
-        const response = await fetch(`${RATING_URL}&s=${ratings}`);
-        const data = await response.json();
-        setRatings(data.Search);
-    }
 
 
     useEffect(() => {
@@ -48,7 +64,6 @@ const App = () => {
                     alt="search"
                     onClick={() => {
                         searchMovies(searchTerm)
-                        searchRatings(searchTerm)
                     }}
                 />
             </div>
